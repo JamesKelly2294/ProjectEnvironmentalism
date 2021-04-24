46083ba6cfb2c89fb51d537f0f0f3f59586ff778
Jacob Ingalls <jacobingalls@yahoo.com>
2021-04-24 15:56:01 -0700

    Add Info card update struct

---
+++ Assets/Game/UI/PrimaryInfoCard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimaryInfoCard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public struct PrimaryInfoCardResourcesUpdate
+{
+    public decimal dollarsAmount, dollarsIncomePerSecond, dolarsExpensesPerSecond;
+    public decimal oilPrice, oilProduced, oilConsumed;
+    public int numberOfDerricks, numberOfAllowedDerricks;
+    public int numberOfRigs, numberOrAllowedRigs;
+    public int numberOfTrucks, numberOfAllowedTrucks;
+    public int numberOfShips, numberOfAllowedShips;
+    public double environmentalHealth;
+    public double publicSentiment;
+
+}
