ceccda691e94129d0d335732285176ed53a2e5d5
Jacob Ingalls <jacobingalls@yahoo.com>
2021-04-24 18:42:51 -0700

    Add listeners for game state changes

--- Assets/Game/UI/PrimaryInfoCard.cs
+++ Assets/Game/UI/PrimaryInfoCard.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using TMPro;
 
 public class PrimaryInfoCard : MonoBehaviour
 {
+
+    public TextMeshProUGUI buisnessName, date;
+    public TextMeshProUGUI dollars, income, expenses;
+    public TextMeshProUGUI oilPrice, oilProduction, oilDemand;
+    public TextMeshProUGUI derricks, rigs, trucks, ships;
 
     public ProgressSlider environmentSlider, sentimentSlider, oilStorageSlider;
 
@@ -20,19 +27,43 @@ public class PrimaryInfoCard : MonoBehaviour
         
     }
 
+    /// Accepts a string
+    public void NameDidChange(PubSubListenerEvent e) {
+        buisnessName.SetText((string) e.value);
+    }
+
+    /// Accepts a DateTime
+    public void DateDidChange(PubSubListenerEvent e) {
+        DateTime newTime = (DateTime)e.value;
+        string timeString = newTime.ToString("m, yyyy");
+        date.SetText(timeString);
+    }
+
     /// Accepts a PrimaryInfoCardResourcesDollarsUpdate
     public void DollarsDidChange(PubSubListenerEvent e) {
         PrimaryInfoCardResourcesDollarsUpdate update = (PrimaryInfoCardResourcesDollarsUpdate)e.value;
+        dollars.SetText(update.dollarsAmount.ToString("N2"));
+        income.SetText(update.dollarsIncomePerSecond.ToString("N2"));
+        expenses.SetText(update.dolarsExpensesPerSecond.ToString("N2"));
     }
 
     /// PrimaryInfoCardResourcesOilUpdate
     public void OilDidChange(PubSubListenerEvent e) {
         PrimaryInfoCardResourcesOilUpdate update = (PrimaryInfoCardResourcesOilUpdate)e.value;
+        oilPrice.SetText(update.oilPrice.ToString("N2"));
+        oilProduction.SetText(update.oilProducedPerSecond.ToString("N2"));
+        oilDemand.SetText(update.oilConsumedPerSecond.ToString("N2"));
     }
 
     /// Accepts a PrimaryInfoCardResourcesEquipmentUpdate
     public void EquipmentDidChange(PubSubListenerEvent e) {
+
         PrimaryInfoCardResourcesEquipmentUpdate update = (PrimaryInfoCardResourcesEquipmentUpdate)e.value;
+        derricks.SetText(update.numberOfDerricks.ToString("N0") + "/" + update.numberOfAllowedDerricks.ToString("N0"));
+        rigs.SetText(update.numberOfRigs.ToString("N0") + "/" + update.numberOrAllowedRigs.ToString("N0"));
+        trucks.SetText(update.numberOfTrucks.ToString("N0") + "/" + update.numberOfAllowedTrucks.ToString("N0"));
+        ships.SetText(update.numberOfShips.ToString("N0") + "/" + update.numberOfAllowedShips.ToString("N0"));
+
     }
 
     // Accepts a double
@@ -58,7 +89,7 @@ public struct PrimaryInfoCardResourcesDollarsUpdate
 
 public struct PrimaryInfoCardResourcesOilUpdate
 {
-    public decimal oilPrice, oilProduced, oilConsumed;
+    public decimal oilPrice, oilProducedPerSecond, oilConsumedPerSecond;
 }
 
 public struct PrimaryInfoCardResourcesEquipmentUpdate
