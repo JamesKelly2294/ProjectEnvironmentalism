f7474e3c0e8217e935154a54a2ec846ebf4f33e1
Jacob Ingalls <jacobingalls@yahoo.com>
2021-04-24 17:56:26 -0700

    Add Oil storage assets

--- Assets/Game/UI/PrimaryInfoCard.cs
+++ Assets/Game/UI/PrimaryInfoCard.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PrimaryInfoCard : MonoBehaviour
 {
+
+    public ProgressSlider environmentSlider, sentimentSlider, oilStorageSlider;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,17 +19,52 @@ public class PrimaryInfoCard : MonoBehaviour
     {
         
     }
+
+    /// Accepts a PrimaryInfoCardResourcesDollarsUpdate
+    public void DollarsDidChange(PubSubListenerEvent e) {
+        PrimaryInfoCardResourcesDollarsUpdate update = (PrimaryInfoCardResourcesDollarsUpdate)e.value;
+    }
+
+    /// PrimaryInfoCardResourcesOilUpdate
+    public void OilDidChange(PubSubListenerEvent e) {
+        PrimaryInfoCardResourcesOilUpdate update = (PrimaryInfoCardResourcesOilUpdate)e.value;
+    }
+
+    /// Accepts a PrimaryInfoCardResourcesEquipmentUpdate
+    public void EquipmentDidChange(PubSubListenerEvent e) {
+        PrimaryInfoCardResourcesEquipmentUpdate update = (PrimaryInfoCardResourcesEquipmentUpdate)e.value;
+    }
+
+    // Accepts a double
+    public void EvironmentalChange(PubSubListenerEvent e) {
+        environmentSlider.progress = ((float) e.value) / 100f;
+    }
+
+    // Accepts a double
+    public void PublicSentimentDidChange(PubSubListenerEvent e) {
+        sentimentSlider.progress = ((float) e.value) / 100f;
+    }
+
+    // Accepts a double
+    public void OilStorageDidChange(PubSubListenerEvent e) {
+        oilStorageSlider.progress = ((float) e.value) / 100f;
+    }
 }
 
-public struct PrimaryInfoCardResourcesUpdate
+public struct PrimaryInfoCardResourcesDollarsUpdate
 {
     public decimal dollarsAmount, dollarsIncomePerSecond, dolarsExpensesPerSecond;
+}
+
+public struct PrimaryInfoCardResourcesOilUpdate
+{
     public decimal oilPrice, oilProduced, oilConsumed;
+}
+
+public struct PrimaryInfoCardResourcesEquipmentUpdate
+{
     public int numberOfDerricks, numberOfAllowedDerricks;
     public int numberOfRigs, numberOrAllowedRigs;
     public int numberOfTrucks, numberOfAllowedTrucks;
     public int numberOfShips, numberOfAllowedShips;
-    public double environmentalHealth;
-    public double publicSentiment;
-
 }
---
+++ Assets/Game/UI/ProgressSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressSlider : MonoBehaviour
+{
+
+    public GameObject needle;
+
+    [Range(0f, 1f)]
+    public float progress;
+    public float padding = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RectTransform needleRect = needle.GetComponent<RectTransform>();
+        RectTransform selfRect = gameObject.GetComponent<RectTransform>();
+        float offset = padding + (progress * (selfRect.rect.width - (2 * padding))) - (needleRect.rect.width / 2);
+        needleRect.localPosition = new Vector3(offset, needleRect.localPosition.y, needleRect.localPosition.z);
+
+    }
+}
