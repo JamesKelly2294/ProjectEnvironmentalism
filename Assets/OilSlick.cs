470d5821d77afeba90388026fcadf072c5839e81
James Kelly <dxk2294@rit.edu>
2021-04-24 19:16:54 -0700

    Begin oil slicks

---
+++ Assets/OilSlick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilSlick : MonoBehaviour
+{
+    public SpriteRenderer OilSlickSprite;
+    public Color NonPurchasableColor = new Color(1, 1, 1, 6.0f / 100.0f);
+    public SelectableSprite Selectable;
+    public bool Purchasable = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Purchasable)
+        {
+            Selectable.BoxHighlightable = false;
+            Selectable.BoxSelectable = false;
+            Selectable.Selectable = false;
+            Selectable.Highlightable = false;
+
+            OilSlickSprite.color = NonPurchasableColor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
