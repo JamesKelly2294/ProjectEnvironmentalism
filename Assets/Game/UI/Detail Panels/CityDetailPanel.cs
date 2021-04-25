b5f5b613eace8643b94bb5a8b412fa6ae147234c
Jacob Ingalls <jacobingalls@yahoo.com>
2021-04-24 23:01:56 -0700

    Show info on they sity you selected

---
+++ Assets/Game/UI/Detail Panels/CityDetailPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CityDetailPanel : MonoBehaviour
+{
+
+    public TextMeshProUGUI title;
+    public Image flag;
+
+    public City city;
+
+    public Sprite flagUS, flagCuba, flagMexico;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void refresh() {
+        title.SetText(city.name);
+
+        if (city.Country == Country.UnitedStates) {
+            flag.sprite = flagUS;
+        } else if (city.Country == Country.Cuba) {
+            flag.sprite = flagCuba;
+        } else if (city.Country == Country.Mexico) {
+            flag.sprite = flagMexico;
+        }
+
+    }
+
+    public void SetCity(City city) {
+        this.city = city;
+        refresh();
+    }
+}
---
+++ Assets/Game/UI/Detail Panels/DetailViewManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetailViewManager : MonoBehaviour
+{
+
+    public CityDetailPanel cityDetailPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DidSelectCity(PubSubListenerEvent e) {
+        City city = ((GameObject) e.value).gameObject.GetComponent<City>();
+        cityDetailPanel.SetCity(city);
+        cityDetailPanel.gameObject.SetActive(true);
+    }
+
+    public void DidDeselectCity(PubSubListenerEvent e) {
+        City city = ((GameObject) e.value).gameObject.GetComponent<City>();
+        if (cityDetailPanel.city.name == city.name) {
+            cityDetailPanel.gameObject.SetActive(false);
+        }
+    }
+}
