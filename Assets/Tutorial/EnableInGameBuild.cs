f1bbcca07e92019217b7d75f1e254b34795ca636
James Kelly <dxk2294@rit.edu>
2021-04-26 10:43:34 -0700

    Add basic tutorial

---
+++ Assets/Tutorial/EnableInGameBuild.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableInGameBuild : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_EDITOR
+        Destroy(gameObject);
+#else
+        for (var i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            child.gameObject.SetActive(true);
+        }
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
